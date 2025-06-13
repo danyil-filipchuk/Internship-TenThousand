03c0194b0b3779473978c142fa21b4168b1df811
danyil-filipchuk <filipchuk.kyiv@gmail.com>
2025-06-13 12:20:53 +0300

    add a few new algorithms in sorting JS file

--- RegExp.js
+++ RegExp.js
@@ -1,6 +1,6 @@
 // 1):
 // const text = 'Hello World1234+';
-
+//
 // const regexp = /[a-z]+/ig;
 // const regexp = /[^0-9]+/ig;
 // const regexp = /\w+/g;
@@ -9,110 +9,110 @@
 
 // 2):
 // const text = '<@Danya/>Hey Team. I would like to have a call with <@Dima123/> at 14:00. <@John/> would you join us?';
-
+//
 // const regexp = /(?<=@)\w+(?=\/)/g;
 // const regexp = /(?<=<@)\w+/g;
 
-// 3):
+// // 3):
 // const text = '<@Danya/>Hey Team. I citykyiv@gmail.com would like to have test@icloud.ua a call with my.email-123@sub.domain.com <@Dima123/> at 14:00. <@John/> would you join us?';
-
+//
 // const regexp = /[\w.-]+@[\w.-]+\.[a-z]{2,12}/g;
 
 // 4):
 // const text = '<@Danya/>Hey Team. I +380664531331 citykyiv@gmail.com +38 (066) 453-13-31 would like 38 (066) 453-13-31 to have test@icloud.ua a call with my.email-123@sub.domain.com <@Dima123/> at 14:00. <@John/> would you join us?';
-
+//
 // const regexp = /(\+?)\d+[0-9\-)(\s]{10,16}(?:\d)/g;
 
 // 5):
 // const text = 'MESS1KXD6549CA CALL1KXD6549USMESS6ZPP0V3LUS, CALLPZDE12S4US, MESSKXS49D16US'
-
+//
 // const regexp = /(mess|call)\w{8}(ca|us)/ig;
 
 // 6):
 // const text = 'citykyiv@kompas.travel';
-
-//const regexp = /^[\w]+@[\w.]+\.[a-z]{2,12}$/
+//
+// const regexp = /^[\w]+@[\w.]+\.[a-z]{2,12}$/
 
 // 7):
 // const text = 'https://www.youtube.com';
-
-//const regexp = /^http(s?):\/\/[\w.\-]+\.[a-z]{2,12}$/g;
+//
+// const regexp = /^http(s?):\/\/[\w.\-]+\.[a-z]{2,12}$/g;
 
 // Особисті завдання (1):
-// const text = "Напишіть на info@example.com або support123@service.org, а також перевірте admin@localhost.";
+const text1 = "Напишіть на info@example.com або support123@service.org, а також перевірте admin@localhost.";
 
-// const result = text.match(/[\w.\-]+@[\w.\-]+(?:\.[a-z]{0,12})?\b/g);
-// console.log(result); // Очікується: ['info@example.com', 'support123@service.org', 'admin@localhost']
+const result1 = text1.match(/[\w.\-]+@[\w.\-]+(?:\.[a-z]{0,12})?\b/g);
+console.log(result1); // Очікується: ['info@example.com', 'support123@service.org', 'admin@localhost']
 
 // Особисті завдання (2):
-// const text = "У мене є 3 яблука, 15 бананів і 1000 гривень.";
+const text2 = "У мене є 3 яблука, 15 бананів і 1000 гривень.";
 
-// const result = text.match(/\d+/g);
-//console.log(result); // Очікується: ['3', '15', '1000']
+const result2 = text2.match(/\d+/g);
+console.log(result2); // Очікується: ['3', '15', '1000']
 
 // Особисті завдання (3):
-// const text = "Привіт <@John123/>, поговори з <@Anna/> і <@Dima_01/>";
+const text3 = "Привіт <@John123/>, поговори з <@Anna/> і <@Dima_01/>";
 
-// const result = text.match(/<@\w+\/>/g);
-//console.log(result); // Очікується: ['<@John123/>', '<@Anna/>', '<@Dima_01/>']
+const result3 = text3.match(/<@\w+\/>/g);
+console.log(result3); // Очікується: ['<@John123/>', '<@Anna/>', '<@Dima_01/>']
 
 // Особисті завдання (4):
-// const text = "Це #тестовий пост з #хештегами, перевірка #Regex_101! #";
+const text4 = "Це #тестовий пост з #хештегами, перевірка #Regex_101! #";
 
-// const result = text.match(/#[\wа-яА-ЯіІїЇєЄґҐ]+/gu);
-// console.log(result); // Очікується: ['#тестовий', '#хештегами', '#Regex_101']
+const result4 = text4.match(/#[\wа-яА-ЯіІїЇєЄґҐ]+/gu);
+console.log(result4); // Очікується: ['#тестовий', '#хештегами', '#Regex_101']
 
 // Особисті завдання (5):
-// const text = "Hello world. This is a Test. Let's Meet at Main Street I.";
+const text5 = "Hello world. This is a Test. Let's Meet at Main Street I.";
 
-// const result = text.match(/[A-Z][a-z]+/g);
-// console.log(result); // Очікується: ['Hello', 'This', 'Test', 'Let', 'Meet', 'Main', 'Street']
+const result5 = text5.match(/[A-Z][a-z]+/g);
+console.log(result5); // Очікується: ['Hello', 'This', 'Test', 'Let', 'Meet', 'Main', 'Street']
 
 
 // Особисті завдання (6):
-// const text = "Мій номер +380501234567, а ще є запасний 063-123-45-67 і 380991234567. Є ще якийсь 1234567 — але це не номер.";
+const text6 = "Мій номер +380501234567, а ще є запасний 063-123-45-67 і 380991234567. Є ще якийсь 1234567 — але це не номер.";
 
-// const result = text.match(/(\+380|380|0)+\d{2}\-?\d{3}\-?\d{2}\-?\d{2}/g);
-// console.log(result); // Очікується: ['+380501234567', '063-123-45-67', '380991234567']
+const result6 = text6.match(/(\+380|380|0)+\d{2}\-?\d{3}\-?\d{2}\-?\d{2}/g);
+console.log(result6); // Очікується: ['+380501234567', '063-123-45-67', '380991234567']
 
 // Особисті завдання (7):
-// const text = "Книга коштує 300 грн, блокнот — 49.99грн, а ручка - 15 грн. Це не ціна: 500 доларів.";
+const text7 = "Книга коштує 300 грн, блокнот — 49.99грн, а ручка - 15 грн. Це не ціна: 500 доларів.";
 
-// const result = text.match(/\d+([.,]\d+)?\s?грн/g);
-// console.log(result); // Очікується: ['300 грн', '49.99грн', '15 грн']
+const result7 = text7.match(/\d+([.,]\d+)?\s?грн/g);
+console.log(result7); // Очікується: ['300 грн', '49.99грн', '15 грн']
 
 // Особисті завдання (8):
-// const text = "Зайди на https://example.com, також дивись http://test.org або www.something.net і просто site.ua";
+const text8 = "Зайди на https://example.com, також дивись http://test.org або www.something.net і просто site.ua";
 
-// const result = text.match(/((http|https):\/\/)?(www\.)?[\w.\-]+[a-z.]{2}/g);
-// console.log(result); // Очікується: ['https://example.com', 'http://test.org', 'www.something.net', 'site.ua']
-
-// Особисті завдання (9):
-// const text = "У стилі використано кольори: #fff, #000000, #AbC123, а іноді некоректні: #12, #GGG, #12345.";
-
-// const result = text.match(/#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})\b/g);
-//console.log(result); // Очікується: ['#fff', '#000000', '#AbC123']
+const result8 = text8.match(/((http|https):\/\/)?(www\.)?[\w.\-]+[a-z.]{2}/g);
+console.log(result8); // Очікується: ['https://example.com', 'http://test.org', 'www.something.net', 'site.ua']
 
 // Особисті завдання (9):
-// const text = "Документація тут: [Google](https://google.com), ще можна глянути [MDN](http://developer.mozilla.org) чи просто [Docs](/local/path). Але це не посилання: [Bad](not a url).";
+const text9 = "У стилі використано кольори: #fff, #000000, #AbC123, а іноді некоректні: #12, #GGG, #12345.";
 
-// const result = text.match(/\[[^\]]+\]\((|http(s?):\/\/|\/)?[^\s]+\)/g);
-// console.log(result); // Очікується: ['[Google](https://google.com)', '[MDN](http://developer.mozilla.org)', '[Docs](/local/path)']
+const result9 = text9.match(/#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})\b/g);
+console.log(result9); // Очікується: ['#fff', '#000000', '#AbC123']
 
 // Особисті завдання (10):
-// const text = "Ось тестові картки: 1234-5678-9012-3456, інша – 9876 5432 1098 7654, а 1111222233334444 – не підходить, як і 1234-5678-9012-345.";
+const text10 = "Документація тут: [Google](https://google.com), ще можна глянути [MDN](http://developer.mozilla.org) чи просто [Docs](/local/path). Але це не посилання: [Bad](not a url).";
 
-// const result = text.match(/\b\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}\b/g);
-// console.log(result); // Очікується: ['1234-5678-9012-3456', '9876 5432 1098 7654']
-
-// Особисті завдання (11):
-// const text = "Сьогодні вилітають рейси: AA1234, BZ9999 і ib0350. Також є кодовийі: ZZ123, XY12345, AB12CD.";
-
-// const result = text.match(/\b[a-zA-Z]{2}\d{4}\b/g);
-// console.log(result); // Очікується: ['AA1234', 'BZ9999', 'ib0350']
+const result10 = text10.match(/\[[^\]]+\]\((|http(s?):\/\/|\/)?[^\s]+\)/g);
+console.log(result10); // Очікується: ['[Google](https://google.com)', '[MDN](http://developer.mozilla.org)', '[Docs](/local/path)']
 
 // Особисті завдання (11):
-const text = `
+const text11 = "Ось тестові картки: 1234-5678-9012-3456, інша – 9876 5432 1098 7654, а 1111222233334444 – не підходить, як і 1234-5678-9012-345.";
+
+const result11 = text11.match(/\b\d{4}[\-\s]\d{4}[\-\s]\d{4}[\-\s]\d{4}\b/g);
+console.log(result11); // Очікується: ['1234-5678-9012-3456', '9876 5432 1098 7654']
+
+// Особисті завдання (12):
+const text12 = "Сьогодні вилітають рейси: AA1234, BZ9999 і ib0350. Також є кодовийі: ZZ123, XY12345, AB12CD.";
+
+const result12 = text12.match(/\b[a-zA-Z]{2}\d{4}\b/g);
+console.log(result12); // Очікується: ['AA1234', 'BZ9999', 'ib0350']
+
+// Особисті завдання (13):
+const text13 = `
   <!DOCTYPE html>
   <html>
     <head>
@@ -127,10 +127,8 @@ const text = `
   </html>
 `;
 
-const result = text.match(/<title>.*?<\/title>/g);
-console.log(result); // Очікується: ['<title>Головна сторінка</title>', '<title>Неправильний тег у тілі</title>']
-
-
+const result13 = text13.match(/<title>.*?<\/title>/g);
+console.log(result13); // Очікується: ['<title>Головна сторінка</title>', '<title>Неправильний тег у тілі</title>']
 
 // \s - пробіл
 // \w - a-zA-Z0-9_
@@ -149,6 +147,6 @@ console.log(result); // Очікується: ['<title>Головна сторі
 // {2} - повинно бути 2 символи
 // {2,12} - кількість символів від 2 до 12
 // (abc|def) - знайти abc або def
-
+//
 // flag i - ігнорувати регістр
 // flag g - глобальний пошук
--- Sorting.js
+++ Sorting.js
@@ -1,156 +1,224 @@
-// // Bubble Sort (Сортування бульбашкою):
-//
-// function bubbleSort(arr){
-//     for (let i = 0; i < arr.length; i++) {
-//         for (let j = 0; j < arr.length - 1 - i; j++) {
-//             if (arr[j] > arr[j+1]) { // або поставити < для сортування у зворотню сторону
-//                 let temp = arr[j];
-//                 arr[j] = arr[j+1];
-//                 arr[j+1] = temp;
-//             }
-//         }
-//     }
-//     return arr;
-// }
-//
-// const array1 = [2,5,42,15,25,10];
-// console.log(bubbleSort(array1));
-//
-// // Selection Sort (Сортування вибором):
-//
-// function selectionSort(arr){
-//     for (let i = 0; i < arr.length-1; i++) {
-//         let minIndex = i;
-//         for (let j = i+1; j < arr.length; j++) {
-//             if (arr[j] < arr[minIndex]) { // або поставити < для сортування у зворотню сторону
-//                 minIndex = j;
-//             }
-//         }
-//         if (minIndex !== i) {
-//             let temp = arr[i];
-//             arr[i] = arr[minIndex];
-//             arr[minIndex] = temp;
-//         }
-//     }
-//     return arr;
-// }
-//
-// const array2 = [2,5,42,15,25,10];
-// console.log(selectionSort(array2));
-//
-// // Insertion Sort (Сортування включенням):
-//
-// function insertionSort(arr){
-//     for (let i = 1; i < arr.length; i++) {
-//         let temp = arr[i];
-//         let j = i - 1;
-//         while (j >= 0 && arr[j] > temp) { // або поставити < для сортування у зворотню сторону
-//             arr[j+1] = arr[j];
-//             j--;
-//         }
-//         arr[j+1] = temp;
-//     }
-//     return arr;
-// }
-//
-// const array3 = [2,5,42,15,25,10];
-// console.log(insertionSort(array3));
-//
-// // Merge Sort (Сортування злиттям):
-//
-// function mergeSort(arr) {
-//     if (arr.length <= 1) {
-//         return arr;
-//     }
-//
-//     const middle = Math.floor(arr.length / 2);
-//     const left = mergeSort(arr.slice(0, middle));
-//     const right = mergeSort(arr.slice(middle));
-//
-//     let i = 0;
-//     let j = 0;
-//     const sortedArr = [];
-//
-//     while (i < left.length && j < right.length) {
-//         if (left[i] === right[j]) {
-//             sortedArr.push(left[i], right[j]);
-//             i++;
-//             j++;
-//         } else if (left[i] < right[j]) { // або поставити < для сортування у зворотню сторону
-//             sortedArr.push(left[i]);
-//             i++;
-//         } else {
-//             sortedArr.push(right[j]);
-//             j++;
-//         }
-//     }
-//
-//     return  sortedArr
-//         .concat(left.slice(i))
-//         .concat(right.slice(j));
-// }
-//
-// const array4 = [2,5,42,15,25,10];
-// console.log(mergeSort(array4));
-//
-// // Quick Sort (Швидке сортування);
-//
-// function quickSort(arr) {
-//     if (arr.length <= 1) {
-//         return arr;
-//     }
-//     const pivot = arr[Math.floor(arr.length / 2)];
-//     const left = arr.filter(n => n < pivot); // або поставити > для сортування у зворотню сторону
-//     const right = arr.filter(n => n > pivot); // або поставити < для сортування у зворотню сторону
-//     const center = arr.filter(n => n === pivot);
-//
-//     return quickSort(left).concat(center, quickSort(right));
-// }
-//
-// const array5 = [2,5,42,15,25,10];
-// console.log(quickSort(array5));
-//
-// // Shell Sort (сортування Шелла);
-//
-// function shellSort(arr) {
-//     for (let step = Math.floor(arr.length / 2); step > 0; step = Math.floor(step / 2)) {
-//         for (let pass = step; pass < arr.length; pass++) {
-//             for(let replacement = pass - step; replacement >= 0 && arr[replacement] > arr[replacement + step]; replacement -= step) {
-//                 let tmp = arr[replacement];
-//                 arr[replacement] = arr[replacement + step];
-//                 arr[replacement + step] = tmp;
-//             }
-//         }
-//     }
-//     return arr;
-// }
-//
-// const array6 = [2,5,42,15,25,10];
-// console.log(shellSort(array6));
-//
-// // Count Sort (сортування підрахунком):
-//
-// function countSort(arr) {
-//     if (arr.length <= 1) {
-//         return arr;
-//     }
-//
-//     const maxValue = Math.max(...arr);
-//     const count = new Array(maxValue + 1).fill(0);
-//
-//     for (const nums of arr) {
-//         count[nums]++;
-//     }
-//
-//     const result = [];
-//     for (let value = 0; value <= maxValue; value++) { // для сортування у зворотню сторону - (let value = maxValue; value >= 0; value--)
-//         const c = count[value];
-//         for (let i = 0; i < c; i++) {
-//             result.push(value);
-//         }
-//     }
-//     return result;
-// }
-//
-// const array8 = [2,5,42,15,25,10];
-// console.log(countSort(array8));
+// Bubble Sort (Сортування бульбашкою):
+
+function bubbleSort(arr){
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - 1 - i; j++) {
+            if (arr[j] > arr[j+1]) { // або поставити < для сортування у зворотню сторону
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+const array1 = [2,5,42,15,25,10];
+console.log(bubbleSort(array1));
+
+// Selection Sort (Сортування вибором):
+
+function selectionSort(arr){
+    for (let i = 0; i < arr.length-1; i++) {
+        let minIndex = i;
+        for (let j = i+1; j < arr.length; j++) {
+            if (arr[j] < arr[minIndex]) { // або поставити < для сортування у зворотню сторону
+                minIndex = j;
+            }
+        }
+        if (minIndex !== i) {
+            let temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+    }
+    return arr;
+}
+
+const array2 = [2,5,42,15,25,10];
+console.log(selectionSort(array2));
+
+// Insertion Sort (Сортування включенням):
+
+function insertionSort(arr){
+    for (let i = 1; i < arr.length; i++) {
+        let temp = arr[i];
+        let j = i - 1;
+        while (j >= 0 && arr[j] > temp) { // або поставити < для сортування у зворотню сторону
+            arr[j+1] = arr[j];
+            j--;
+        }
+        arr[j+1] = temp;
+    }
+    return arr;
+}
+
+const array3 = [2,5,42,15,25,10];
+console.log(insertionSort(array3));
+
+// Merge Sort (Сортування злиттям):
+
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const middle = Math.floor(arr.length / 2);
+    const left = mergeSort(arr.slice(0, middle));
+    const right = mergeSort(arr.slice(middle));
+
+    let i = 0;
+    let j = 0;
+    const sortedArr = [];
+
+    while (i < left.length && j < right.length) {
+        if (left[i] === right[j]) {
+            sortedArr.push(left[i], right[j]);
+            i++;
+            j++;
+        } else if (left[i] < right[j]) { // або поставити < для сортування у зворотню сторону
+            sortedArr.push(left[i]);
+            i++;
+        } else {
+            sortedArr.push(right[j]);
+            j++;
+        }
+    }
+
+    return  sortedArr
+        .concat(left.slice(i))
+        .concat(right.slice(j));
+}
+
+const array4 = [2,5,42,15,25,10];
+console.log(mergeSort(array4));
+
+// Quick Sort (Швидке сортування);
+
+function quickSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+    const pivot = arr[Math.floor(arr.length / 2)];
+    const left = arr.filter(n => n < pivot); // або поставити > для сортування у зворотню сторону
+    const right = arr.filter(n => n > pivot); // або поставити < для сортування у зворотню сторону
+    const center = arr.filter(n => n === pivot);
+
+    return quickSort(left).concat(center, quickSort(right));
+}
+
+const array5 = [2,5,42,15,25,10];
+console.log(quickSort(array5));
+
+// Shell Sort (сортування Шелла);
+
+function shellSort(arr) {
+    for (let step = Math.floor(arr.length / 2); step > 0; step = Math.floor(step / 2)) {
+        for (let pass = step; pass < arr.length; pass++) {
+            for(let replacement = pass - step; replacement >= 0 && arr[replacement] > arr[replacement + step]; replacement -= step) {
+                let tmp = arr[replacement];
+                arr[replacement] = arr[replacement + step];
+                arr[replacement + step] = tmp;
+            }
+        }
+    }
+    return arr;
+}
+
+const array6 = [2,5,42,15,25,10];
+console.log(shellSort(array6));
+
+// Count Sort (сортування підрахунком):
+
+function countSort(arr) {
+    if (arr.length <= 1) {
+        return arr;
+    }
+
+    const maxValue = Math.max(...arr);
+    const count = new Array(maxValue + 1).fill(0);
+
+    for (const nums of arr) {
+        count[nums]++;
+    }
+
+    const result = [];
+    for (let value = 0; value <= maxValue; value++) { // для сортування у зворотню сторону - (let value = maxValue; value >= 0; value--)
+        const c = count[value];
+        for (let i = 0; i < c; i++) {
+            result.push(value);
+        }
+    }
+    return result;
+}
+
+const array7 = [2,5,42,15,25,10];
+console.log(countSort(array7));
+
+// Heap Sort (пірамидальне сортування):
+
+function heapSort(arr) {
+    const n = arr.length;
+
+    function heapify(i, heapSize) {
+        let largest = i;
+        const left  = 2 * i + 1;
+        const right = 2 * i + 2;
+
+        if (left < heapSize && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < heapSize && arr[right] > arr[largest]) {
+            largest = right;
+        }
+
+        if (largest !== i) {
+            [arr[i], arr[largest]] = [arr[largest], arr[i]];
+            heapify(largest, heapSize);
+        }
+    }
+
+    for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+        heapify(i, n);
+    }
+
+    for (let heapSize = n; heapSize > 1; heapSize--) {
+        [arr[0], arr[heapSize - 1]] = [arr[heapSize - 1], arr[0]];
+        heapify(0, heapSize - 1);
+    }
+    return arr;
+}
+
+// Приклад використання
+const array8 = [2,5,42,15,25,10];
+console.log(heapSort(array8));
+
+// Radix Sort (порозрядне сортування):
+
+function radixSort(arr) {
+
+    let position = Array(10).fill(0).map(value => []);
+    let multiplier = 1;
+    let maxValue = Math.max(...arr);
+
+    while(maxValue-multiplier>=0) {
+        arr.forEach(value => {
+                let slice = Math.floor(value/multiplier);
+                position[slice % 10].push(value);
+            }
+        )
+        let indexValue = 0;
+        position.forEach( values => {
+                for(let i = 0; values.length > 0; i++){
+                    arr[indexValue] = values.shift(0);
+                    indexValue++;
+                }
+            }
+        )
+        multiplier*=10;
+    }
+    return arr;
+}
+
+const array9 = [2,5,42,15,25,10];
+console.log(radixSort(array9));
